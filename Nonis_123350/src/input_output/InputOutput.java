877878a5761f2a64191db8710591a1f520cb0051
Andrea <37176672+DarkHelmet28@users.noreply.github.com>
2019-10-25 12:10:57 +0200

    input random random random

---
+++ Nonis_123350/src/input_output/InputOutput.java
@@ -0,0 +1,42 @@
+package input_output;
+
+import random.RandomGenerator;
+
+
+public class InputOutput {
+
+   
+    /**
+     * Metodo che crea una stringa di input formata da numeri double random di lunghezza prefissata
+     * @param lunghezza numero di double nella stringa
+     * @param hardSeed seed per calcolo del Random
+     * @return stringa di numeri double
+     */
+    public String makeInput(int lunghezza, int hardSeed) {
+        String input = "";
+        for(int i = 0; i < lunghezza; i++) {
+            input += makeNumeroRandom(hardSeed) + " , ";
+        }
+        input = input.substring(0, input.length() - 2);
+        input += " .";
+        return input;
+    }
+
+    /**
+     * Metodo che genera un double casuale con precisione di 5 cifre decimali e lo trasforma in stringa
+     * @param hardSeed seed reale per il calcolo del seedRandom
+     * @return Stringa di un singolo double.
+     */
+    public String makeNumeroRandom(int hardSeed) {
+        int seedRandom = (int)(Math.random() * hardSeed);
+        RandomGenerator random = new RandomGenerator(seedRandom);
+        double maxNum = random.get() * 1000000.0;
+        double realNum = random.get() * maxNum;
+        // Arrotonda double a 5 cifre decimali
+        double realNumRound = Math.round(realNum * 100000d) / 100000d;
+        String realNumString = realNumRound + "";
+        return realNumString;
+    }
+
+
+}
--- Nonis_123350/src/main_time/MainTime.java
+++ Nonis_123350/src/main_time/MainTime.java
@@ -1,9 +1,15 @@
 package main_time;
 
+import input_output.InputOutput;
+
 public class MainTime {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        InputOutput inOut = new InputOutput();
+
         System.out.println("Main Time");
+        System.out.println(inOut.makeInput(50, 57524145));
+
     }
 
 }
