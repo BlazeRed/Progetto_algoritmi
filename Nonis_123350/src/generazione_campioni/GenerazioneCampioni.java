b1fc5f755de112419d9d71ccb3cf9e8d2fc42b51
Andrea <37176672+DarkHelmet28@users.noreply.github.com>
2019-10-28 11:18:31 +0100

    gen campioni

---
+++ Nonis_123350/src/generazione_campioni/GenerazioneCampioni.java
@@ -0,0 +1,19 @@
+package generazione_campioni;
+
+public class GenerazioneCampioni {
+
+    int lunghezza;
+    int ripetizioni;
+    double za;
+
+    public GenerazioneCampioni(int lung) {
+        lunghezza = lung;
+        ripetizioni = 10;
+        za = 2.33;
+    }
+
+    
+
+
+// End Class
+}
